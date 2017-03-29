750ff56098b95dcfd3fa9e5fedab6fa327b8ad14
Jan Nörtemann <jan.noertemann@xamla.com>
2017-03-29 19:19:47 +0200

    Update Action Client Server Tests

--- ActionServerSample/Program.cs
+++ ActionServerSample/Program.cs
@@ -15,29 +15,30 @@ namespace ActionServerSample
             ROS.Init(new string[0], "ActionServer");
             NodeHandle serverNodeHandle = new NodeHandle();
             Console.WriteLine("Create server");
-            var actionServer = new ActionServer<SingleJointPositionGoal, SingleJointPositionResult,
-                SingleJointPositionFeedback>(serverNodeHandle, "ActionTest");
+            var actionServer = new ActionServer<Messages.actionlib.TestGoal, Messages.actionlib.TestResult,
+                Messages.actionlib.TestFeedback>(serverNodeHandle, "test_action");
             Console.WriteLine("Start Server");
             actionServer.Start();
 
-            bool goalRegistered = false;
             actionServer.RegisterGoalCallback((goalHandle) =>
             {
-                Console.WriteLine($"Goal registered callback. Max velo: {goalHandle.Goal.max_velocity}");
-                var fb = new SingleJointPositionFeedback();
-                fb.velocity = 10.0;
+                Console.WriteLine($"Goal registered callback. Goal: {goalHandle.Goal.goal}");
+                var fb = new Messages.actionlib.TestFeedback();
+                fb.feedback = 10;
                 goalHandle.PublishFeedback(fb);
                 Thread.Sleep(100);
+                var result = new Messages.actionlib.TestResult();
+                result.result = 123;
                 goalHandle.SetGoalStatus(Messages.actionlib_msgs.GoalStatus.SUCCEEDED, "done");
-                goalRegistered = true;
+                actionServer.PublishResult(goalHandle.GoalStatus, result);
             });
 
-            Console.WriteLine("Wait for action server receiving the goal");
-            while (!goalRegistered)
+
+            while (!Console.KeyAvailable)
             {
                 Thread.Sleep(1);
             }
-
+            actionServer.Shutdown();
             serverNodeHandle.shutdown();
             ROS.shutdown();
         }
